d7f5baa7ba703715174c63c59aa2d85dc878648f
kaizhu94 <zhukai0803@gmail.com>
2020-12-07 14:28:14 -0500

    Update User_controller

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -9,10 +9,25 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token **
 
+attr_reader :password
 
 def reset_session_token!
     self.update!(session_token: self.class.generate_session_token)
     self.session_token
+end
+
+def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+end
+
+def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+end
+
+def self.find_credentials(username, password)
+    user = User.find_by(username: username )
+    return nil unless user && user.is_password?(password)
 end
 
 
