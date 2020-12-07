0aa6408919a506d65b3ae21bf06924bd692d2daa
kaizhu94 <zhukai0803@gmail.com>
2020-12-07 15:36:33 -0500

    Update new

--- skeleton/app/controllers/application_controller.rb
+++ skeleton/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @user ||= Users.find_by(session_token: session[:session_token])
+    end
+
+    def 
+    
 end
